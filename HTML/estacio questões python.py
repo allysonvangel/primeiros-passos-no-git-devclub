3cfaea81f85fb2150ac9fe359cc1acd8e6196a13
allysonvangel <vangel.allyson@gmail.com>
2024-02-17 11:32:27 -0300

    primeiro commit assistindo DevClub tirar a foto de verdade.17/02/2024

---
+++ HTML/estacio questões python.py
@@ -0,0 +1,19 @@
+class NoArvore:
+    def __init__(self, chave = None, esquerda = None, direita = None):
+        self.chave = chave
+        self.esquerda = esquerda
+        self.direita = direita
+
+if __name__ == '__main__':
+    raiz = NoArvore(55)
+
+    raiz.esquerda = NoArvore(35)
+    raiz.direita  = NoArvore(75)
+
+    raiz.direita.esquerda  = NoArvore(65)
+    raiz.direita.direita   = NoArvore(85)
+    raiz.esquerda.esquerda = NoArvore(25)
+    raiz.esquerda.direita  = NoArvore(45)
+
+
+   
